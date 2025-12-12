ec533a9055e6dc616c2a40c96fe5a4e7c60a7c8b
OSS-Star <ladvi298@gmail.com>
2025-12-13 00:38:54 +0100

    update

--- components/PlanningGrid.tsx
+++ components/PlanningGrid.tsx
@@ -578,7 +578,7 @@ export default function PlanningGrid({ onUpdateStats, onOpenCallModal }: Plannin
                 label={`SAUVEGARDER`}
                 icon={<Save size={14} />}
                 green
-                className="rounded-none shadow-[0_0_15px_rgba(34,197,94,0.3)] px-8"
+                className="rounded-full cursor-pointer shadow-[0_0_15px_rgba(34,197,94,0.3)] px-8"
               />
             </div>
           )}
